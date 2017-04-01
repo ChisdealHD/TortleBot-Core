ed7523ed8853d4ba6a2fe0187efe5ec2e7fec808
Bas Kuipers <bas.kuipers@me.com>
2017-04-01 22:33:02 +0200

    Fuck

--- modules/music.js
+++ modules/music.js
@@ -99,7 +99,7 @@ const music = function(TortleBot) {
 
     TortleBot.addTraditionalCommand('np', message => {
         if(typeof nowPlaying[message.guild.id] !== 'undefined' && typeof nowPlaying[message.guild.id] !== null && message.guild.voiceConnection) {
-            message.reply("Now playing: " + nowPlaying[message.guild.i].url)
+            message.reply("Now playing: " + nowPlaying[message.guild.id].url)
         }else {
             message.reply("Currently idle.")
         }
