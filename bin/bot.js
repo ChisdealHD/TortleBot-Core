d1471f9778dd101e6b532d6cc751e07d39462b45
Bas Kuipers <bas.kuipers@me.com>
2017-04-02 21:20:07 +0200

    Music is now a seperate module

--- bin/bot.js
+++ bin/bot.js
@@ -6,7 +6,7 @@ const TortleBot = require('../TortleBot')
 const bot = new TortleBot(process.env.BOT_TOKEN || process.argv[2])
 
 // bot.registerModule(require('../modules/channellink.js'))
-bot.registerModule(require('../modules/music.js'))
+bot.registerModule(require('tortlebot-music'))
 bot.registerModule(require('../modules/goplay.js'))
 bot.registerModule(require('../modules/utilities.js'))
 
